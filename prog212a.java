33a46db47d390858c7cf2016d0498823cba8c0eb
Malcolm <speedylol@users.noreply.github.com>
2015-11-13 14:02:53 -0500

    Update prog212a.java

--- prog212a.java
+++ prog212a.java
@@ -1,55 +1,44 @@
-/*
-	Passing Trains
-	Malcolm Speed
-	Nov. 10, 2014
-	Manjaro Linux
-
-	Description:
-		calculate when two trains pass
-
-*/
-
 import java.util.Scanner;
 
 public class prog212a {
 
-	static Scanner input = new Scanner(System.in);
-	public static void main(String[] args) {
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
 
-		// array to keep track of the time
-		int[] time = { 9, 10, 11, 12, 1, 2};
-		int passTrain = 0;
-		int freightTrain = 40;
-		int i = 0; int i2 = 0;
+        // array to keep track of the time
+        int[] time = {9, 10, 11, 12, 1, 2};
+        int passTrain = 0;
+        int freightTrain = 0;
+        int i = 0; int i2 = 0;
 
-		System.out.print("Enter the speed of the passengers train ");
-		int user = input.nextInt();			
-		passTrain = user;
-
-		do {
-			i2++;
-			i = i2;
-			i = time[i];
-	
-			freightTrain += 40;
-			System.out.println(passTrain + " " + freightTrain);
-	
-			if(i == 11)
-				passTrain += user;
-			
-		} while (passTrain <= 160 && freightTrain <= 160);
-		
-		int i3 = i + 1;
-
-		if(passTrain >= 120 && freightTrain >= 120) {
-			if (passTrain > freightTrain) {
-				System.out.printf("passed at %d and %d", i, i3);
-				System.exit(0);
-			}
-			System.out.print(i);
-		}
-
-		
-			
-	}
+        System.out.print("Enter the speed of the passengers train: ");
+        int user = input.nextInt();         
+        
+        do {
+            freightTrain += 50;
+            
+            if(i >= 11) 
+                passTrain += user;
+            
+            i2++;
+            i = i2;
+            i = time[i];            
+            
+            //System.out.println(passTrain + " " + freightTrain); 
+            
+            if(passTrain == freightTrain) {
+                System.out.printf("passed at %d",  time[i2]);
+                System.out.println("The freight train was traveling 50 mph");
+                System.out.println("The passenger train was traveling " + user + " mph");
+                System.exit(0);
+            }       
+                    
+        } while (passTrain <= 200 && freightTrain <= 200);
+        
+        
+        System.out.printf("passed at %d and %d 'o clock\n", i-1,  time[i2]);
+        System.out.println("The freight train was traveling 50 mph");
+        System.out.println("The passenger train was traveling " + user + " mph");
+        System.exit(0);
+    }
 }
